ea5f8dd0adc88d83e3acfdb622a1830a372ff868
farhan <farhanmohammad8214@gmail.com>
2022-10-15 18:53:12 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementsByTagName('container');
+const container = document.querySelector('#container');
+document.querySelectorAll('second');
+document.querySelector('third');
+const ol = document.querySelector('ol');
+ol.querySelectorAll('.third');
+container.setAttribute("text","Hello!")
+const foot = document.querySelector('.footer');
+foot.setAttribute("class","main");
+const newLi = document.createElement("li");
+newLi.innerText = "four"
+const newUl = document.querySelector("ul");
+newUl.append(newLi);
+greenLi= ol.querySelectorAll('li');
+for(li of greenLi)
+{
+	li.style.color = "green";
+}
+foot.remove;
+
+
+
